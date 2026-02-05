1150ec1a911c778868bb90d9a4692a0a62231852
Torben Leth <SShadowS@SShadowS.dk>
2026-02-05 15:42:16 +0100

    Fix URI parsing for paths with spaces
    URL-encode special characters (spaces, brackets) in path_to_uri to
    prevent lsp_types::Uri parser from rejecting paths like "Base Application".
    
    Bump version to 0.2.2

--- src/protocol.rs
+++ src/protocol.rs
@@ -37,9 +37,22 @@ pub fn uri_to_path(uri: &Uri) -> Option<PathBuf> {
 pub fn path_to_uri(path: &Path) -> Uri {
     let path_str = path.to_string_lossy();
     #[cfg(windows)]
-    let uri_str = format!("file:///{}", path_str.replace('\\', "/"));
+    let path_normalized = path_str.replace('\\', "/");
     #[cfg(not(windows))]
-    let uri_str = format!("file://{}", path_str);
+    let path_normalized = path_str.to_string();
+
+    // URL-encode special characters (spaces, brackets, etc.)
+    let path_encoded = path_normalized
+        .replace(' ', "%20")
+        .replace('(', "%28")
+        .replace(')', "%29")
+        .replace('[', "%5B")
+        .replace(']', "%5D");
+
+    #[cfg(windows)]
+    let uri_str = format!("file:///{}", path_encoded);
+    #[cfg(not(windows))]
+    let uri_str = format!("file://{}", path_encoded);
 
     match uri_str.parse() {
         Ok(uri) => uri,
